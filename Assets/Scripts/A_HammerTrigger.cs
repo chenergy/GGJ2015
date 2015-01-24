aab91153bb2805ee7a898048e7b917d5b4838a42
Jonathan Chien <jonathan.chien@gmail.com>
2015-01-24 10:21:35 -0800

    added basic hammering functionality

---
+++ Assets/Scripts/A_HammerTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class A_HammerTrigger : MonoBehaviour
+{
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.GetComponent <Hammer> () != null) {
+			this.OnHammerHit ();
+		}
+	}
+
+	protected abstract void OnHammerHit ();
+}
+
---
+++ Assets/Scripts/HammerTrigger_Button.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ButtonState {
+	BUTTON_UP,
+	BUTTON_DOWN
+}
+
+public class HammerTrigger_Button : A_HammerTrigger
+{
+	public Sprite up;
+	public Sprite down;
+
+	public ButtonState state = ButtonState.BUTTON_DOWN;
+
+	private SpriteRenderer spriteRenderer;
+
+
+
+	void Start (){
+		this.spriteRenderer = this.GetComponent <SpriteRenderer> ();
+
+		if (state == ButtonState.BUTTON_DOWN) {
+			this.spriteRenderer.sprite = down;
+		} else {
+			this.spriteRenderer.sprite = up;
+		}
+	}
+
+	protected override void OnHammerHit ()
+	{
+		Debug.Log ("Hammer has hit button");
+	}
+}
+
