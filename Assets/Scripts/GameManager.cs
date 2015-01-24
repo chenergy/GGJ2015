a21646c269f05247e7e95e12baddd8e370cd7a61
Jonathan Chien <jonathan.chien@gmail.com>
2015-01-23 21:04:13 -0800

    first commit!

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CurrentLevel {
+	BOSS_BATTLE,
+	GRAVITY,
+	BOMB_DISARM
+}
+
+
+public class GameManager : MonoBehaviour {
+	private static GameManager instance = null;
+	public static GameManager Instance {
+		get { return instance; }
+	}
+
+	public CurrentLevel level = CurrentLevel.BOSS_BATTLE;
+
+
+	void Awake (){
+		if (instance != null) {
+			GameObject.Destroy (this.gameObject);
+		} else {
+			instance = this;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
