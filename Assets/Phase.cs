9541dcafa069c57c33563d89696bdc66d04396b2
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-01-24 00:15:02 -0800

    add basic phase script; add to a few walls

---
+++ Assets/Phase.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Phase : MonoBehaviour {
+
+	private bool canPhase = true;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) && canPhase) {
+			collider2D.isTrigger = true;
+			gameObject.renderer.material.color = Color.black;
+			canPhase = false;
+			Invoke("unphase", 2);
+			Invoke("cooldown", 6);
+		}
+	}
+
+	void unphase() {
+		gameObject.renderer.material.color = Color.white;
+		collider2D.isTrigger = false;
+	}
+
+	void cooldown() {
+		canPhase = true;
+	}
+}
