cb3f3eb4e4c1fb05a4930c8e76af6331fbb9ea35
jjlee88 <jjlee88@stanford.edu>
2015-01-25 12:29:02 -0800

    script to start enemy. also tried to make an activator.

---
+++ Assets/Scripts/HammerablActivator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HammerablActivator : Hammer {
+
+	// Use this for initialization
+	protected override void Start () {
+		this.isHammering = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
